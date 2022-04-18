443d83b585f2e9b5ab8432cce23961c0ce6ab22e
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2022-04-18 12:29:55 +0300

    Update highest_score.py

--- highest_score.py
+++ highest_score.py
@@ -5,8 +5,14 @@ with open('train_eli5.json') as f_in:
     obj = json.loads(line)
     scores.append(float(obj['score']))
     
+scores = sorted(scores)
 print (max(scores))
 print(min(scores))
 print(sum(scores)/len(scores))
 print(len(scores))
-print(sorted(scores)[len(scores)//2])
+print(scores[len(scores)//2])
+print(scores[len(scores)-1])
+print(scores[len(scores)-2])
+print(scores[len(scores)-3])
+print(scores[len(scores)-4])
+
