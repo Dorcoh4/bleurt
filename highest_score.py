a6d5205ff6b96b8224296779b0934413cc6d66ce
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2022-04-18 12:15:33 +0300

    Create highest_score.py

---
+++ highest_score.py
@@ -0,0 +1,11 @@
+import json
+scores = []
+with open('train_eli5.json') as f_in:
+  for line in f_in:
+    obj = json.loads(line)
+    scores.append(float(obj['score']))
+    
+print (max(scores))
+print(min(scores))
+print(sum(scores)/len(scores))
+print(len(scores))
