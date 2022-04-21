19dea7ccdedd76561c888c072c55fde4423c9b6e
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2022-04-21 21:51:02 +0300

    Update other_splits.py

--- other_splits.py
+++ other_splits.py
@@ -32,8 +32,9 @@ with open('manual_questions.csv', 'r') as the_file:
   lines = the_file.readlines()
   for line in lines:
     print (f'line:{line}')
-#     for i, x in enumerate(candidates):
-#       print(f'candidate = {x}')
+    for i, x in enumerate(candidates):
+      if "there any car crashes that make the car run faster afterward" in x:
+        print(f'candidate = {x}')
     local_indices = [i for i, x in enumerate(candidates) if x.replace('\n','\\n') in line]
     print(len(local_indices))
     new_references += [references[i] for i in local_indices]
