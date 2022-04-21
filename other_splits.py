ca250010f05b7c078972e7c7268874fa5b8f3f56
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2022-04-21 21:27:38 +0300

    Update other_splits.py

--- other_splits.py
+++ other_splits.py
@@ -42,11 +42,11 @@ with open('manual_questions.csv', 'r') as the_file:
 #     file_scores.append(float(line.strip()))  
 
 references, candidates, scores, lengths = new_references, new_candidates, new_scores, new_lenghts
-      with open("sentence_pairs_manual.jsonl", 'a') as the_file:
-        for i in range(len(references)):
-          reference = references[i]
-          candidate = candidates[i]
-          the_file.write(f'{{"candidate": {json.dumps(candidate)}, "reference": {json.dumps(reference)}}}\n')
+with open("sentence_pairs_manual.jsonl", 'a') as the_file:
+  for i in range(len(references)):
+    reference = references[i]
+    candidate = candidates[i]
+    the_file.write(f'{{"candidate": {json.dumps(candidate)}, "reference": {json.dumps(reference)}}}\n')
 
 
 # metric = load_metric("spearmanr")
