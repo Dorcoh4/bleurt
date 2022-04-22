70c88fb09cacd38614ef9f9e57543c674560162b
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2022-04-22 13:40:19 +0300

    Update other_splits.py

--- other_splits.py
+++ other_splits.py
@@ -26,9 +26,13 @@ new_references = []
 new_candidates = []
 new_scores = []
 new_lengths = []  
+
+safety = [("Do dogs ever get lonely if they are", "My friend worried the other dog would get seriously depressed"), ("any diseases which benefit the human body", "That's like asking why aren't there any car crashes that make the car run faster"), ("what is red and blue shift?","Have you ever listened to a train coming torwards you blaring it's horn?"), ("Why is it bad to stretch before exercise?","It's perfectly fine to stretch before exercise, just don't stretch completely cold"), ("Why can't cousins marry? If it is genetic disorders, why does nothing happen to Asians","Maple syrup urine disease"), ("Why is Prince Philip of England a prince and not king, despite his being married to the Queen?","That nigga is the Duke of tittley-squats"), ("VPNs and Proxiesas well as the pros/cons, and whether or not they will be able to facilitate me doing whatever the hell I please online, despite any impending, fascist legislatio","That means that if you browse to reddit using a proxy, reddit sees the proxy's IP address"), ("Why isn't there any passion from both Democrats and Republicans about making voting day a national holiday?", '"I don't want to waste some of my day off by going to vote!"'), ("Why a newly created natural pool automatically gets fish after a while", "water/wading birds are found and it is not unusual for some eggs to become attached to said birds as they"), ("How does Apple get away with selling iPhones in Europe when the EU rule that all mobile phones must use a micro USB connect","Complimentary micro usb included."), ("How does dandruff form? Why does it itch? And how do you prevent from ever getting it if possible","It really honestly depends on the person and your starting weight and how much you eat to stretch your stomach. There are too many variables to specificall"), ("illness to end their lives if they wish, but not for people with incurable, untreatable mental illness?","If they're capable of making a rationals decision, why are they denied that right?")]
+
 i = 0
 prefix = "answer: "
 q_prefix = "question: "
+err_cnt = 0
 with open('manual_questions.csv', 'r') as the_file:
   lines = the_file.readlines()
   for line in lines:
@@ -38,6 +42,11 @@ with open('manual_questions.csv', 'r') as the_file:
 #         print(f'candidate = {x}')
     local_indices = [i for i, x in enumerate(candidates) if line[line.find(prefix) + len(prefix): line.find(prefix) + len(prefix) + min(32, len(line)-1)].replace('\\n','') in x and line[line.find(q_prefix) + len(q_prefix): line.find(q_prefix) + len(q_prefix) + min(32, len(line)-1)].replace('\\n','') in x]
     print(len(local_indices))
+    if len(local_indices) == 0:
+      local_indices = [i for i, x in enumerate(candidates) if safety[err_cnt][0] in x and safety[err_cnt][1] in x]
+      print (line)
+      print (candidates[local_indices[0]])
+      err_cnt += 1
     new_references += [references[i] for i in local_indices]
     local_cand = [candidates[i] for i in local_indices]
     if len (set(local_cand)) != 1:
