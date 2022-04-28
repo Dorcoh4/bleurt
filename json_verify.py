2a7c316b630873d4079c55b024e7c94dc585605a
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2022-04-28 12:26:08 +0300

    Update json_verify.py

--- json_verify.py
+++ json_verify.py
@@ -1,7 +1,10 @@
 import json
 with open("sentence_pairs_manual.jsonl", 'r') as the_file:
-  for line in the_file:
-    try:
-      json.loads(line)
-    except:
-      print("An exception occurred: " + line)
+  with open("tmp.jsonl", 'w') as to_file:
+    for line in the_file:
+      try:
+        json.loads(line)
+        to_file.write(line)
+      except:
+        to_file.write(line[1:])
+        print("An exception occurred: " + line)
