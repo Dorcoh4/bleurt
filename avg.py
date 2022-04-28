4c506e98a373ed9e9a185bc14d326f1118e793ad
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2022-04-28 20:19:03 +0300

    Create avg.py

---
+++ avg.py
@@ -0,0 +1,12 @@
+import sys
+
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+file_name = sys.argv[1]
+score = []
+with open(file_name, 'r') as the_file:  
+  for line in the_file:
+    scores.append(float(line.strip()))
+    
+    
+print (f"{len(scores)}  -> {sum(scores)/len(scores)}")
