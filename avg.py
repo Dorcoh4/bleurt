5d6e8273cd81573d17e5fd55a4109017ac57a80c
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2022-04-29 13:31:16 +0300

    Update avg.py

--- avg.py
+++ avg.py
@@ -1,10 +1,22 @@
 import sys
+import json
 
 file_name = sys.argv[1]
+file_name2 = sys.argv[2]
 scores = []
-with open(file_name, 'r') as the_file:  
-  for line in the_file:
-    scores.append(float(line.strip()))
-    
+with open(file_name, 'r') as in_file:  
+  with open(file_name2, 'r') as out_file:  
+    in_lines = in_file.readLines()
+    out_lines = out_file.readLines()
+    assert len(out_lines) == len(in_lines)
+    i = 0
+    while i < len(out_lines):
+      local_score = [float(out_lines[i].strip())]
+      candidate = json.loads(in_lines[i])["candidate"]
+      i += 1
+      while candidate == json.loads(in_lines[i])["candidate"]:
+        local_score.append(float(out_lines[i].strip()))
+        i += 1
+      scores.append(max(local_score))
     
 print (f"{len(scores)}  -> {sum(scores)/len(scores)}")
